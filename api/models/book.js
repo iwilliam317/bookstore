f844a90580e816a6316b7b56b69b9d0a400f7a54
william <willshinji@gmail.com>
2018-06-09 12:51:46 -0300

    creates model Book

---
+++ api/models/book.js
@@ -0,0 +1,18 @@
+const mongoose = require('../database');
+
+const BookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    require: true
+  },
+  author: {
+    type: String
+  },
+  release_year: {
+    type: Date
+  }
+});
+
+
+const Book = mongoose.model('Book', BookSchema);
+module.exports = Book;
