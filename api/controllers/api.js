43e3de63d6de7105641f67872e7e7259d4158a51
william <willshinji@gmail.com>
2018-06-10 12:13:08 -0300

    adds nodemon

--- api/controllers/api.js
+++ api/controllers/api.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 
+const Book = require('../models/book');
+
 router.get('/books', async (request, response) => {
   response.send('api funcionando');
 });
