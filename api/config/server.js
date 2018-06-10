25200a235690504d89786e787caaac5c36f5bce1
william <willshinji@gmail.com>
2018-06-10 12:27:56 -0300

    sets middleware bodyParser

--- api/config/server.js
+++ api/config/server.js
@@ -1,4 +1,8 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 global.port = 3000;
 
