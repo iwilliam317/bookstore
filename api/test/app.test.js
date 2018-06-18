6fc5319049121c1a0059c7809fdec755d9f6c5cb
william <willshinji@gmail.com>
2018-06-18 00:08:45 -0300

    creates book for testing

--- api/test/app.test.js
+++ api/test/app.test.js
@@ -8,7 +8,14 @@ const expect = chai.expect;
 
 const app = require('../app.js');
 
+const Book = require('../models/book');
+
 describe('API', () => {
+
+  before((done)=>{
+    Book.create({title: 'My title', author: 'Random Author', release_year: 2018, category: 'Education'});
+    done();
+  });
   describe('#GET /api/books', () => {
 
     it('should return status code 200', done => {
