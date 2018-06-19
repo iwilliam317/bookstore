fb7f00382c41196e2c8a76755eac9efe01300e06
william <willshinji@gmail.com>
2018-06-19 01:46:17 -0300

    adds more specs for put

--- api/test/app.test.js
+++ api/test/app.test.js
@@ -44,7 +44,7 @@ describe('API', () => {
       done();
     });
 
-    it('should return book created', done => {
+    it('should returns book created', done => {
       chai.request(app).get('/api/books')
         .end((error, response) => {
           expect(response.body.book[0].title).to.be.equal('My title');
@@ -71,12 +71,26 @@ describe('API', () => {
   describe('#PUT /api/books/:id', () => {
     it('should return a status code 200', done => {
         const id = book._id;
-        chai.request(app).get(`/api/books/${id}`)
+        chai.request(app).put(`/api/books/${id}`)
           .end((error, response) => {
             expect(response.status).to.be.equal(200);
             expect(response.ok).to.be.true;
           });
       done();
+    });
+
+    it('should updates the book', done => {
+        const id = book._id;
+        chai.request(app).put(`/api/books/${id}`)
+          .end((error, response) => {
+            Book.update({ _id: id }, { title: 'My updated title' }, (error, result) => {
+              // console.log(result);
+              expect(result.ok).to.be.equal(1);
+              expect(result.nModified).to.be.equal(1);
+              expect(result.n).to.be.equal(1);
+            });
+          });
+      done();      
     });
   });
 });
