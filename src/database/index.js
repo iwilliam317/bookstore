db98318a3ab76e01195d8fd532eecccefdc3b39d
william <willshinji@gmail.com>
2018-11-24 01:19:23 -0200

    fix for heroku

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/bookstore';
-mongoose.connect(url);
+const mongoURI = 'mongodb://localhost:27017/bookstore';
+mongoose.connect(process.env.MONGOLAB_URI || mongoURI);
 
 mongoose.Promise = global.Promise;
 
