32ff83464c20d3a9969da972577532b08b58274a
william <willshinji@gmail.com>
2018-11-27 15:11:21 -0200

    fix mongoose connection on heroku

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
+const uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost:27017/bookstore';
 
-const mongoURI = 'mongodb://localhost:27017/bookstore';
-mongoose.connect(process.env.MONGOLAB_URI || mongoURI);
+mongoose.connect(uristring,  (err, res) => {
+      if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      } else {
+      console.log ('Succeeded connected to: ' + uristring);
+      }
+  });
 
 mongoose.Promise = global.Promise;
 
