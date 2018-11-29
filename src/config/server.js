b606fd93cd84647b5e82e53e0bc1029e0965b4e1
william <willshinji@gmail.com>
2018-11-29 02:01:46 -0200

    minor adjustments

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
