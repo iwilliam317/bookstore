d394afcc00623851ab583c3e59b1d03b01a8cf4e
william <willshinji@gmail.com>
2018-11-30 14:11:01 -0200

    config cors

--- src/config/server.js
+++ src/config/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 module.exports = app;
 
